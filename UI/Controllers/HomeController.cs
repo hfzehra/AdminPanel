946133766119e3cffe18e6ad2eb52cf6eb3a66dc
fatma zehra uysal <hfzehra.uysal33@gmail.com>
2024-12-18 22:08:16 +0300

    UI pages updated

--- UI/Controllers/HomeController.cs
+++ UI/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace UI.Controllers
             return View();
         }
 
+        public IActionResult Category()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryAdded()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
